a7f6808a3f63d05daed2fc987bbbe7fd3aad0e3f
hubert.rola <hubert.rola1337@gmail.com>
2024-10-08 13:16:53 +0200

    commit

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -12,6 +12,50 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
+    
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Calculator(string op, double? a, double? b)
+    {
+        if (a is null || b is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format w parametrze a lub b";
+            return View("CustomError");
+        }
+        ViewBag.a = a;
+        ViewBag.b = b;
+
+        if (op != "add" || op != "sub" || op != "nul" || op != "div")
+        {
+            ViewBag.ErrorMessage = "Niepoprawny operator!";
+            return View("CustomError");
+        }
+        
+        switch (op)
+        {
+            case Operator.Add:
+                ViewBag.result = a + b;
+                ViewBag.op = "+";
+                break;
+            case Operator.Sub:
+                ViewBag.result = a - b;
+                ViewBag.op = "-";
+                break;
+            case Operator.Div:
+                ViewBag.result = a * b;
+                ViewBag.op = "*";
+                break;
+            case Operator.Mul:
+                ViewBag.result = a / b;
+                ViewBag.op = "/";
+                break;
+        }
+        return View();
+    }
 
     public IActionResult Index()
     {
@@ -27,5 +71,10 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    
+    public enum Operator
+    {
+        Add, Sub, Mul, Div
     }
 }
